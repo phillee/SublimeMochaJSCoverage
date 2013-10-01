7b29c28347f07bef0e0b7d8160458d83509bb29d
Sergey Generalov <sergey@genbit.ru>
2013-10-01 15:42:11 -0700

    Use spaces and pep8 formating tool

--- SublimeJSCoverage.py
+++ SublimeJSCoverage.py
@@ -10,106 +10,106 @@ COVERAGE_DIR_NAME = 'coverage'
 
 
 def find_project_root(file_path):
-	"""
-		Project Root is defined as the parent directory that contains a directory called 'coverage'
-	"""
-	if os.access(os.path.join(file_path, COVERAGE_DIR_NAME), os.R_OK):
-		return file_path
-	parent, current = os.path.split(file_path)
-	if current:
-		return find_project_root(parent)
+    """
+        Project Root is defined as the parent directory 
+        that contains a directory called 'coverage'
+    """
+    if os.access(os.path.join(file_path, COVERAGE_DIR_NAME), os.R_OK):
+        return file_path
+    parent, current = os.path.split(file_path)
+    if current:
+        return find_project_root(parent)
 
 
 def find_coverage_filename(coverage_dir):
-	"""
-		Returns latest coverage json for specifed file or None if cannot find it
-		in specifed coverage direcotry
-	"""
-	files = os.listdir(coverage_dir)
-	debug(",".join(files))
-	getmtime = lambda key: os.path.getmtime(os.path.join(coverage_dir, key))
-	coverage_file_name = None
-	if files:
-		files.sort(key=getmtime, reverse=True)
-		coverage_file_name = files.pop(0)
+    """
+        Returns latest coverage json for specifed file or None if cannot find it
+        in specifed coverage direcotry
+    """
+    files = os.listdir(coverage_dir)
+    debug(",".join(files))
+    getmtime = lambda key: os.path.getmtime(os.path.join(coverage_dir, key))
+    coverage_file_name = None
+    if files:
+        files.sort(key=getmtime, reverse=True)
+        coverage_file_name = files.pop(0)
 
-	return coverage_file_name
+    return coverage_file_name
 
 
 def read_coverage_report(file_path):
-	try:
-		coverage_json = json.load(open(file_path, 'r'))
-		return coverage_json
-	except IOError:
-		return None
+    try:
+        coverage_json = json.load(open(file_path, 'r'))
+        return coverage_json
+    except IOError:
+        return None
 
 
 class ShowJsCoverageCommand(sublime_plugin.TextCommand):
 
-	"""
-		Highlight uncovered lines in the current file
-		based on a previous coverage run.
-	"""
+    """
+        Highlight uncovered lines in the current file
+        based on a previous coverage run.
+    """
+    def run(self, edit):
+        view = self.view
+        # get name of currently opened file
+        filename = view.file_name()
+        if not filename:
+            return
+        project_root = find_project_root(filename)
 
-	def run(self, edit):
-		view = self.view
-		# get name of currently opened file
-		filename = view.file_name()
-		if not filename:
-			return
-		project_root = find_project_root(filename)
+        if not project_root:
+            if view.window():
+                sublime.status_message("Could not find coverage directory.")
+            return
 
-		if not project_root:
-			if view.window():
-				sublime.status_message("Could not find coverage directory.")
-			return
+        relative_filename = filename.replace(project_root + "/", "")
+        coverage_dir = os.path.join(project_root, COVERAGE_DIR_NAME)
+        coverage_filename = find_coverage_filename(coverage_dir)
 
-		relative_filename = filename.replace(project_root + "/", "")
-		coverage_dir = os.path.join(project_root, COVERAGE_DIR_NAME)
-		coverage_filename = find_coverage_filename(coverage_dir)
+        debug("Display js coverage report for file", filename)
+        debug("project_root", project_root)
+        debug("relative_filename", relative_filename)
+        debug("coverage_filename", coverage_filename)
 
-		debug("Display js coverage report for file", filename)
-		debug("project_root", project_root)
-		debug("relative_filename", relative_filename)
-		debug("coverage_filename", coverage_filename)
+        if not coverage_filename:
+            if view.window():
+                sublime.status_message(
+                    "Can't find the coverage file in project root" + str(project_root))
+            return
 
-		if not coverage_filename:
-			if view.window():
-				sublime.status_message(
-					"Can't find the coverage file in project root" + str(project_root))
-			return
+        # Clean up
+        view.erase_status("SublimeJSCoverage")
+        view.erase_regions("SublimeJSCoverage")
 
-		# Clean up
-		view.erase_status("SublimeJSCoverage")
-		view.erase_regions("SublimeJSCoverage")
+        outlines = []
 
-		outlines = []
+        report = read_coverage_report(
+            os.path.join(coverage_dir, coverage_filename))
+        debug("Found report for the following number of files: " + str(len(report)))
 
-		report = read_coverage_report(
-			os.path.join(coverage_dir, coverage_filename))
-		debug("Found report for the following number of files: " + str(len(report)))
+        if not report:
+            view.set_status("SublimeJSCoverage", "UNCOVERED!")
+            if view.window():
+                sublime.status_message(
+                    "Can't find the coverage json file in project root" + project_root)
 
-		if not report:
-			view.set_status("SublimeJSCoverage", "UNCOVERED!")
-			if view.window():
-				sublime.status_message(
-					"Can't find the coverage json file " + file_path)
+        for tested_file_name in report:
+            tested_file_name2 = tested_file_name.replace("./", "")
+            if tested_file_name2 != relative_filename:
+                continue
+            debug("Found test report for file " + str(relative_filename))
+            file_report = report.get(tested_file_name)
+            lines = file_report.get("l")
+            for num in lines:
+                runs = int(lines.get(num))
+                num = int(num) - 1
+                if runs == 0:
+                    region = view.full_line(view.text_point(num, 0))
+                    outlines.append(region)
 
-		for tested_file_name in report:
-			tested_file_name2 = tested_file_name.replace("./", "")
-			if tested_file_name2 != relative_filename:
-				continue
-			debug("Found test report for file " + str(relative_filename))
-			file_report = report.get(tested_file_name)
-			lines = file_report.get("l")
-			for num in lines:
-				runs = int(lines.get(num))
-				num = int(num) - 1
-				if runs == 0:
-					region = view.full_line(view.text_point(num, 0))
-					outlines.append(region)
-
-		# update highlighted regions
-		if outlines:
-			view.add_regions('SublimeJSCoverage', outlines,
-							 'markup.deleted.diff', 'bookmark', sublime.DRAW_OUTLINED)
+        # update highlighted regions
+        if outlines:
+            view.add_regions('SublimeJSCoverage', outlines,
+                             'markup.deleted.diff', 'bookmark', sublime.DRAW_OUTLINED)
